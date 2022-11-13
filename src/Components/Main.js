ba5254b4b3f3beecd03839f37b73000a9eaeef27
faeze <mohadesfaeze@gmail.com>
2022-11-13 16:35:25 +0330

    changing style

--- src/Components/Main.js
+++ src/Components/Main.js
@@ -12,10 +12,10 @@ function Main() {
   useEffect(() => {
     setLoading(true);
     axios
-      .get("https://mp4.ir/api/getworldcup")
+      .get("https://mp4.ir/api/newworldcup.json?v=1")
       .then((response) => {
-        console.log(response);
-        setShow(response.data);
+        console.log(response.data);
+        setShow(response.data.matches);
       })
       .finally(() => setLoading(false));
   }, []);
@@ -32,109 +32,21 @@ function Main() {
             </Col>
           </Row>
 
-          {show.map((item, index) => (
-            <Row key={index} className="d-flex justify-content-center">
-              <Col lg="10" md="10" sm="10" xs="11">
-                <Card className="text-center mt-3 bg-dark">
-                  {item.isplaying ? (
-                    <a
-                      href={item.link}
-                      style={{
-                        textDecoration: "none",
-                        color: "white",
-                        display: "flex",
-                      }}
-                    >
-                      <Card.Body className=" Card-Body d-flex justify-content-between">
-                        {/* right */}
-                        <div
-                          className="d-flex  align-items-center"
-                          style={{ width: "35%" }}
-                        >
-                          <img
-                            src={`https://mp4.ir/api/flags/${item.flag1}`}
-                            alt="flag1"
-                            className="logo img-fluid"
-                          />
-                          <div className="title1">
-                            <span>{item.title1}</span>
-                          </div>
-                        </div>
-                        {/* center */}
-                        <div
-                          className="d-flex  flex-column text-center align-items-center justify-content-center"
-                          style={{ width: "30%" }}
-                        >
-                          <p className="py-2">در حال پخش</p>
-                          <img
-                            src={play}
-                            alt=""
-                            className="play-logo"
-                            style={{ transform: "translateY(-30%)" }}
-                          />
-                        </div>
-                        {/* left */}
-                        <div
-                          className="d-flex  align-items-center justify-content-end"
-                          style={{ width: "35%" }}
-                        >
-                          <div className="title2">
-                            <span>{item.title2}</span>
-                          </div>
-                          <img
-                            src={`https://mp4.ir/api/flags/${item.flag2}`}
-                            alt="flag1"
-                            className="logo img-fluid"
-                          />
-                        </div>
-                      </Card.Body>
-                    </a>
-                  ) : (
-                    <Card.Body className="Card-Body d-flex justify-content-between ">
-                      {/* right */}
-                      <div
-                        className="d-flex  align-items-center"
-                        style={{ width: "30%" }}
-                      >
-                        <img
-                          src={`https://mp4.ir/api/flags/${item.flag1}`}
-                          alt="flag1"
-                          className="logo img-fluid"
-                        />
-                        <div className="title1">
-                          <span>{item.title1}</span>
-                        </div>
-                      </div>
-                      {/* center */}
-                      <div
-                        className="d-flex flex-column justify-content-center text-center align-items-center"
-                        style={{ width: "30%" }}
-                      >
-                        <div className="d-flex flex-column justify-content-center mt-3 ">
-                          <p>{item.date}</p>
-                          <p>{item.time}</p>
-                        </div>
-                      </div>
-                      {/* left */}
-                      <div
-                        className="d-flex  align-items-center justify-content-end"
-                        style={{ width: "30%" }}
-                      >
-                        <div className="title2">
-                          <span>{item.title2}</span>
-                        </div>
-                        <img
-                          src={`https://mp4.ir/api/flags/${item.flag2}`}
-                          alt="flag1"
-                          className="logo img-fluid"
-                        />
-                      </div>
-                    </Card.Body>
-                  )}
-                </Card>
-              </Col>
-            </Row>
-          ))}
+          {show &&
+            show.map((item, index) => (
+              <Row key={index} className="d-flex justify-content-center">
+                <Col lg="10" md="10" sm="10" xs="11">
+                  <h4 style={{ color: "grey" }}>{item.date}</h4>
+                  {item.data.map((sub, id) => (
+                    <div key={id}>
+                      <h5>{sub.title1}</h5>
+                      <h5>{sub.title2}</h5>
+                    </div>
+                  ))}
+                  <hr />
+                </Col>
+              </Row>
+            ))}
         </Container>
       )}
     </div>
