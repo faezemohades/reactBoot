f2629867fe20f5b106b336fdb54930791906de39
faeze <mohadesfaeze@gmail.com>
2022-11-15 09:27:42 +0330

    last changes

--- src/Components/Main.js
+++ src/Components/Main.js
@@ -26,24 +26,119 @@ function Main() {
         <Loading />
       ) : (
         <Container className="my-5 text-light">
-          <Row className="d-flex justify-content-center mb-4">
-            <Col lg="10" md="10" sm="10" xs="11">
-              <h3>لیست برنامه ها</h3>
+          <Row className="d-flex justify-content-center mt-4">
+            <Col lg="12" md="10" sm="10" xs="11">
+              <h3 >لیست برنامه ها</h3>
             </Col>
           </Row>
 
           {show &&
             show.map((item, index) => (
               <Row key={index} className="d-flex justify-content-center">
-                <Col lg="10" md="10" sm="10" xs="11">
-                  <h4 style={{ color: "grey" }}>{item.date}</h4>
+                <Col lg="12" md="10" sm="10" xs="11">
+                  <h6 style={{ color: "grey" }} className="mt-5 ">{item.date}</h6>
                   {item.data.map((sub, id) => (
-                    <div key={id}>
-                      <h5>{sub.title1}</h5>
-                      <h5>{sub.title2}</h5>
-                    </div>
+                    <Row key={id} className="d-flex justify-content-center">
+                    <Col >
+                      <Card className="text-center mt-3 bg-dark">
+                        {sub.isplaying ? (
+                          <a
+                            href={sub.link}
+                            style={{
+                              textDecoration: "none",
+                              color: "white",
+                              display: "flex",
+                            }}
+                          >
+                            <Card.Body className=" Card-Body d-flex justify-content-between">
+                              {/* right */}
+                              <div
+                                className="d-flex  align-items-center"
+                                style={{ width: "35%" }}
+                              >
+                                <img
+                                  src={`https://mp4.ir/api/flags/${sub.flag1}`}
+                                  alt="flag1"
+                                  className="logo img-fluid"
+                                />
+                                <div className="title1">
+                                  <span>{sub.title1}</span>
+                                </div>
+                              </div>
+                              {/* center */}
+                              <div
+                                className="d-flex  flex-column text-center align-items-center justify-content-center"
+                                style={{ width: "30%" }}
+                              >
+                                <p className="py-2">در حال پخش</p>
+                                <img
+                                  src={play}
+                                  alt=""
+                                  className="play-logo"
+                                  style={{ transform: "translateY(-30%)" }}
+                                />
+                              </div>
+                              {/* left */}
+                              <div
+                                className="d-flex  align-items-center justify-content-end"
+                                style={{ width: "35%" }}
+                              >
+                                <div className="title2">
+                                  <span>{sub.title2}</span>
+                                </div>
+                                <img
+                                  src={`https://mp4.ir/api/flags/${sub.flag2}`}
+                                  alt="flag1"
+                                  className="logo img-fluid"
+                                />
+                              </div>
+                            </Card.Body>
+                          </a>
+                        ) : (
+                          <Card.Body className="Card-Body d-flex justify-content-between ">
+                            {/* right */}
+                            <div
+                              className="d-flex  align-items-center"
+                              style={{ width: "30%" }}
+                            >
+                              <img
+                                src={`https://mp4.ir/api/flags/${sub.flag1}`}
+                                alt="flag1"
+                                className="logo img-fluid"
+                              />
+                              <div className="title1">
+                                <span>{sub.title1}</span>
+                              </div>
+                            </div>
+                            {/* center */}
+                            <div
+                              className="d-flex flex-column justify-content-center text-center align-items-center"
+                              style={{ width: "30%" }}
+                            >
+                              <div className="d-flex flex-column justify-content-center mt-3 ">
+                                 <p>{sub.time}</p>
+                              </div>
+                            </div>
+                            {/* left */}
+                            <div
+                              className="d-flex  align-items-center justify-content-end"
+                              style={{ width: "30%" }}
+                            >
+                              <div className="title2">
+                                <span>{sub.title2}</span>
+                              </div>
+                              <img
+                                src={`https://mp4.ir/api/flags/${sub.flag2}`}
+                                alt="flag1"
+                                className="logo img-fluid"
+                              />
+                            </div>
+                          </Card.Body>
+                        )}
+                      </Card>
+                    </Col>
+                  </Row>
                   ))}
-                  <hr />
                 </Col>
               </Row>
             ))}
